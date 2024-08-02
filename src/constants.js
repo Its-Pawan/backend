d8483bbe238faefe122542ecfbb94cf1a0b0d433
Pawan Joshi <business.pawanjoshi@gmail.com>
2024-08-02 15:02:00 +0530

    work on utils file

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "pawan"
+export const LIMIT = "16kb"
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import 'dotenv/config'
+import { app } from './app.js';
 import connectDB from "./db/index.js";
 
 connectDB()
+    .then(() => {
+        const Port = process.env.PORT || 3000
+        app.on("error", err => console.error("Samething went wrong with app !!!", err))
+        app.listen(Port, () => console.log("Server is running at post: ", Port))
+    })
+    .catch(err => console.log("MongoDB connection failed !!!!", err))

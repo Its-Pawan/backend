517032dc0950ded1e12e865f7613d69bbf132c33
Pawan Joshi <business.pawanjoshi@gmail.com>
2024-08-08 14:44:17 +0530

    Create user controller and routes etc

--- src/app.js
+++ src/app.js
@@ -15,4 +15,16 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+
+// Routes import------>
+
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+// ex url => localhost:3000/api/v1/users/register 
+
+
 export { app }
